8d8a13aa3ef502e49485592992de58bbfc400839
Mazhar Khan <66962188+khanmazhar@users.noreply.github.com>
2020-08-26 22:29:34 -0700

    updated the converter

--- converter.js
+++ converter.js
@@ -1,15 +1,29 @@
-function convertToRoman(num) {
- var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
- var romanNumeral = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+document.querySelector('.invalid').style.display = 'none';
+document.querySelector('#convert').addEventListener('click', function () {
+    var input = document.querySelector('#in-num').value;
 
-var roman = '';
- for (var i = 0; i < decimalValue.length; i++){
-   while(decimalValue[i] <= num) {
-     roman += romanNumeral[i];
-     num-=decimalValue[i];
-   }
- }
- return roman;
+    var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var romanNumeral = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+    var roman = '';
+
+    for (var i = 0; i < decimalValue.length; i++) {
+        while (decimalValue[i] <= input) {
+            roman += romanNumeral[i];
+            input -= decimalValue[i];
+        }
+    }
+
+    if (roman === '') {
+        document.querySelector('.invalid').style.display = 'block';
+        document.querySelector('#in-num').addEventListener('keyup', displayNone)
+    } else {
+        document.querySelector('#in-num').value = roman;
+        document.querySelector('#in-num').addEventListener('keyup', displayNone);
+    }
+
+})
+
+function displayNone() {
+    document.querySelector('.invalid').style.display = 'none';
 }
-
-convertToRoman(12);
